5686324cac4091771d9f0feae679eec3cd5a3ca0
Rama AL-khatib <21220009@htu.edu.jo.com>
2024-01-08 05:52:16 +0300

    add object

--- Basket/Demo.java
+++ Basket/Demo.java
@@ -5,9 +5,10 @@ package Basket;
 public class Demo {
     public static void main(String[] args) {
         Basket<Double, Double> b = new Basket<>();
-        b.add(2.3);
-        b.add(2.7);
-b.add(3.0);
-      b.add(7.0);
+        b.add(2.4);
+        b.add(5.6);
+        System.out.println(b.toString());
+        
+       
     }
 }
