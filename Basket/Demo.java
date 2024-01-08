f4df7f26c415e0087ee9f9289fe2c631ad31841f
Rama AL-khatib <21220009@htu.edu.jo.com>
2024-01-08 06:24:35 +0300

    rama first edit

--- Basket/Demo.java
+++ Basket/Demo.java
@@ -7,9 +7,9 @@ public class Demo {
         Basket<Double, Double> b = new Basket<>();
         b.add(2.4);
         b.add(5.6);
-        System.out.println(b.toString());
-	b.add(8.0);
-	System.out.println(b.toString());
+b.add(6.6);
+
+      
 	
         
        
